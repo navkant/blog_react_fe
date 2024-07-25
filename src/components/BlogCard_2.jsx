4a48ca2587ab9a87253d20b6e84a333adf6ec3db
navkant <navkant.tyagi9@gmail.com>
2024-07-25 07:53:30 +0530

    added width issue on blogCard

--- src/components/BlogCard_2.jsx
+++ src/components/BlogCard_2.jsx
@@ -10,9 +10,9 @@ const BlogCard = (props) => {
 
   if (props.blogData.is_latest) {
     return (
-      <Link to={"/blogs/" + props.blogData.id}>
-        <div>
-          <div className="flex justify-between mx-40 my-5 h-96 bg-gray-100 hover:bg-gray-200 rounded-3xl">
+      <Link key={props.blogData.id} to={"/blogs/" + props.blogData.id}>
+        <div className="flex ">
+          <div className="flex justify-between m-auto my-5 w-3/4 h-80 bg-gray-100 hover:bg-gray-200 rounded-3xl">
             <div className="flex flex-col justify-between m-4">
               <div className="flex flex-col">
                 <div className="w-16 px-2 bg-black text-white rounded-md">
@@ -29,13 +29,43 @@ const BlogCard = (props) => {
                   <h3>{props.blogData.author_name}</h3>
                 </div>
               </div>
-              <div>
-                <h1>{props.blogData.content.slice(0, 100)}</h1>
+              <div className="flex">
+                <div>
+                  <h1>{props.blogData.content.slice(0, 100)}</h1>
+                </div>
               </div>
             </div>
 
-            <div className="w-64 m-16">
-              <img className="" src={props.blogData.image_url} alt="" />
+            <div>
+              <div className="flex justify-end m-2">
+                {accessAuthToken() &&
+                accessUserDetails().user_id === props.blogData.author_id ? (
+                  <div className="m-2">
+                    <Link to="/editor" state={props.blogData}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                        />
+                      </svg>
+                    </Link>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+
+              <div className="p-4 m-4">
+                <img className="" src={props.blogData.image_url} alt="" />
+              </div>
             </div>
           </div>
         </div>
@@ -45,7 +75,7 @@ const BlogCard = (props) => {
     return (
       <Link key={props.blogData.id} to={"/blogs/" + props.blogData.id}>
         <div className="flex ">
-          <div className="flex justify-between mx-40 my-5 h-80 bg-gray-100 hover:bg-gray-200 rounded-3xl">
+          <div className="flex justify-between m-auto my-5 w-3/4 h-80 bg-gray-100 hover:bg-gray-200 rounded-3xl">
             <div className="flex flex-col justify-between m-4">
               <div className="flex flex-col">
                 <div className=" m-1">
