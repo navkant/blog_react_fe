1e1c6f251b1599d6ea2f8188a12c2e6d186a4c51
navkant <navkant.tyagi@thoughtworks.com>
2025-01-02 10:59:10 +0530

    made the backend secure

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const DEV_URL = "http://18.208.164.200";
+export const DEV_URL = "https://21f0z1gdni.execute-api.us-east-1.amazonaws.com/production";
 export const LOCAL_URL = "http://localhost:8000";
